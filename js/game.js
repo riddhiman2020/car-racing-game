4e583028805e59fd2936051ee9a6d9a3d86baf4f
riddhiman2020 <74648005+riddhiman2020@users.noreply.github.com>
2021-03-07 12:58:44 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,24 @@
+class Game{
+    constructor(){
+
+    }
+    getState(){
+        var gameStateref=database.ref("gameState")
+        gameStateref.on("value",function(data){
+            gameState=data.val();
+        })
+    }
+    update(State){
+        database.ref("/").update({
+            gameState:State
+        })
+    }
+    start(){
+        if(gameState===0){
+            player=new Player()
+            player.getCount()
+            form=new Form()
+            form.display()
+        }
+    }
+}
